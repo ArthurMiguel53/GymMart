022173499ea449789d265309a799e09c7e618108
Diênifer <dienifersiqueira.dev@gmail.com>
2023-09-27 17:02:55 -0300

    Commit: Adição do Componente BrandLogo e Estilos
    Feito por: yDienifer
    Data: 27/09/2023
    
    Resumo das Alterações:
    - Criei os seguintes arquivos:
      - BrandLogo.vue
      - styles.css
      - base.css
      - colorPallete.css
    - Criei as seguintes pastas:
      - assets
      - styles
      - fonts
    
    Detalhes das Alterações:
    - No arquivo 'BrandLogo.vue' dentro da pasta 'BrandLogo':
      - Criei o componente da logo do website.
      - Utilizei 'gsap' para criar a animação.
      - Utilizei TypeScript para adicionar tipagem às variáveis.
      - Instalei o FontAwesome para adicionar o ícone da logo.
      - Utilizei o GSAP para fazer a animação.
    
    - No arquivo 'style.css':
      - Estilizei o componente 'BrandLogo'.
      - Defini as cores de '.highlighted-letter' e '#brand-logo-icon' usando as cores root definidas no arquivo 'colorPalette.css'.
      - Implementei a responsividade da fonte da logo e do ícone usando a propriedade 'clamp()'.
    
    - No arquivo 'color-palette.css':
      - Adicionei a paleta de cores do site, que consiste em um total de 4 cores.

---
+++ src/components/BrandLogo/BrandLogo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="brand-logo-container">
+        <span>G</span>
+        <span>Y</span>
+        <span>M</span>
+        <font-awesome-icon :icon="['fas', 'dumbbell']" id="brand-logo-icon" />
+        <span class="highlighted-letter">M</span>
+        <span class="highlighted-letter">A</span>
+        <span class="highlighted-letter">R</span>
+        <span class="highlighted-letter">T</span>
+    </div>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+library.add(faDumbbell);
+
+import { gsap, TweenMax } from 'gsap';
+
+export default defineComponent({
+    name: 'BrandLogo',
+    components: {
+        FontAwesomeIcon,
+    },
+    mounted() {
+        const BRAND_LOGO_ICON: HTMLElement | null = document.getElementById("brand-logo-icon");
+        const HIGHLIGHTED_LETTER: NodeListOf<HTMLElement> = document.querySelectorAll(".highlighted-letter");
+
+        if (BRAND_LOGO_ICON) {
+            setTimeout(() => {
+                gsap.to(HIGHLIGHTED_LETTER, {
+                    left: 'auto',
+                    duration: 1.5,
+                    ease: 'power2.inOut',
+                    onComplete: () => {
+                        gsap.to(BRAND_LOGO_ICON, {
+                            opacity: 1,
+                            duration: 1,
+                            ease: "bounce.out",
+                            top: "auto"
+                        });
+                    }
+                })
+            }, 1000);
+        }
+    }
+});
+</script>
+  
+<style scoped lang="css" src="./style.css"></style>
+  
