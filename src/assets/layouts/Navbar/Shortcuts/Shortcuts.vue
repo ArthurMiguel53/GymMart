17a00bad09767a1422dd64de6d73175e07e7493f
Diênifer <dienifersiqueira.dev@gmail.com>
2023-09-29 18:11:52 -0300

    Commit: Refatoração da Estrutura de Componentes e Estilos
    Feito por: yDienifer
    Data: 29/09/2023
    
    Resumo das Alterações:
    - Refatoração da estrutura de arquivos:
      - App.vue
      - BrandLogo.vue
      - styles.css
      - main.css
      - tsconfig.json
    
    - Adicionados os seguintes arquivos:
      - scattered-forcefields.png
      - NavbarShortcuts.json
      - Navbar.vue
      - Shortcuts.vue
      - styles.css
    
    - Criadas as seguintes pastas:
      - img
      - json
      - layouts
      - Navbar
      - Shortcuts
      - ActionButton
    
    Detalhes das Alterações:
    - No arquivo 'App.vue':
      - Refatoração da estrutura para transferir a criação da navbar para o novo componente 'Navbar.vue'.
      - Importação do componente 'Navbar.vue'.
    
    - Nos arquivos 'BrandLogo.vue' e 'styles.css' dentro da pasta 'ProductCard':
      - Remoção do TweenMax das importações do 'gsap'.
    
    - Na pasta 'img':
      - Adição da imagem 'scattered-forcefields.png', que serve background-image do body da página.
    
    - Nos arquivos 'Shortcuts.vue' e 'styles.css' dentro da pasta 'Shortcuts':
      - Criação de um componente de atalho com props para personalizar o título do atalho e o link de redirecionamento.
      - Conclusão da estilização com 90% da responsividade implementada.
    
    - No arquivo 'NavbarShortcuts.json' dentro da pasta 'json':
      - Adição das informações de id, nome e link dos atalhos usados na navbar.
    
    - Nos arquivos 'Navbar.vue' e 'styles.css' dentro da pasta 'Navbar':
      - Importação do componente BrandLogo e sua inclusão na navbar.
      - Importação dos atalhos do componente Shortcuts e renderização com base no arquivo 'NavbarShortcuts.json'.
      - Substituição das imagens 'cart.png', 'login.png' e 'lupa.png' pelos icons do 'Font Awesome Free'.
      - Alteração da cor de fundo da navbar e da borda.
      - Redução da largura da navbar para 95%, centralizando-a na página.
      - Simplificação e otimização do código CSS.
      - Substituição de seletores '#categories' e '#icons' por '.categories' e '.icons'.
    
    - No arquivo 'main.css':
      - Importação da fonte 'Lato' e aplicação dela ao body da página.
      - Adição da imagem 'scattered-forcefields.png' no background-image do body, em adição de propriedades como background-size e background-position.
    
    - No arquivo 'tsconfig.json':
      - Adição da opção 'resolveJsonModule' com valor 'true' nas 'compilerOptions'.

---
+++ src/assets/layouts/Navbar/Shortcuts/Shortcuts.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul>
+            <li><router-link :to="ShortcutLink"></router-link>{{ ShortcutName }}</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "Shortcuts",
+    props: {
+        ShortcutLink: {
+            type: String,
+            required: true
+        },
+        ShortcutName: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
+
+<style scoped lang="css" src="./styles.css"></style>
