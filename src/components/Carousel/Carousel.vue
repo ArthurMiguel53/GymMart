c824cb74fad374dd4f1df91e1db7fa605ffb8334
Diênifer <dienifersiqueira.dev@gmail.com>
2023-10-03 17:43:03 -0300

    Feito por: yDienifer Data: 03/10/2023
    Commit: Adição do Componente de Banner de Carrossel e Estilização
    
    Resumo das Alterações:
    - Refatoração da estrutura de arquivos:
      - Adicionados os seguintes arquivos:
        - Carousel.vue
        - styles.css
        - CarouselPanel.vue
        - styles.css
        - ProductInterface.ts
      - Criadas as seguintes pastas:
        - Carousel
        - CarouselPanel
        - interfaces
    
    Detalhes das Alterações:
    
    - Nos arquivos 'Carousel.vue' e 'styles.css' dentro da pasta 'src/components/Carousel':
      - Adicionado um container de carrossel no arquivo de template.
      - Implementado um loop para iterar pelos produtos e exibir o componente 'TopBanner' apenas para o produto correspondente ao 'currentSlideIndex'.
      - O componente 'TopBanner' agora recebe informações do produto como propriedades.
      - Adicionado um container de controles do carrossel com o componente 'CarouselPanel' para exibir detalhes dos produtos e permitir a interação com o carrossel.
      - Importados os componentes 'TopBanner' e 'CarouselPanel' e definido o componente 'Carousel' com props 'products' e o estado 'currentSlideIndex'.
      - Implementado o método 'exchangeBannerInfo' para atualizar o 'currentSlideIndex' com o índice do produto selecionado pelos controles do carrossel.
      - Estilização do componente completa.
    
    - Nos arquivos 'CarouselPanel.vue' e 'styles.css' dentro da pasta 'src/components/Carousel/CarouselPanel':
      - Adicionado 'productDetails' como uma prop que contém informações sobre os produtos a serem exibidos no carrossel.
      - Implementado um loop que itera pelos produtos em 'productDetails' e exibe uma imagem de cada produto. Cada imagem é clicável e chama o método 'clickedControlProductIndex' ao ser clicada.
      - Adicionado uma prop 'currentProductId' que representa o ID do produto atualmente em exibição.
      - Implementado o método 'clickedControlProductIndex' que emite um evento 'clickedControlProductIndex' com o índice do produto clicado.
      - Estilos de componente concluídos.
    
    - No arquivo 'ProductInterface.ts' dentro da pasta 'src/components/Carousel/interfaces':
      - Definidas propriedades para representar informações de um produto, como imagem, título, descrição, preço, etc.

---
+++ src/components/Carousel/Carousel.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="carousel-container">
+        <div v-for="(product, index) in products" :key="product.id">
+            <div v-if="index === currentSlideIndex">
+                <TopBanner :productImage="product.productImage" :productAlt="product.productAlt"
+                    :productTitle="product.productTitle" :productDescription="product.productDescription"
+                    :productPrice="product.productPrice" :actionButtonLink="product.actionButtonLink"
+                    :actionButtonTitle="product.actionButtonTitle" :backgroundImage1440px="product.backgroundImage1440px"
+                    :backgroundImage1024px="product.backgroundImage1024px" />
+            </div>
+        </div>
+    </div>
+    <div class="carousel-controls-container">
+        <CarouselPanel :productDetails="products" @clickedControlProductIndex="exchangeBannerInfo" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Product } from "./interfaces/ProductInterface";
+
+import TopBanner from "../TopBanner/TopBanner.vue";
+import CarouselPanel from './CarouselPanel/CarouselPanel.vue';
+
+export default defineComponent({
+    name: "Carousel",
+    components: {
+        TopBanner,
+        CarouselPanel
+    },
+    props: {
+        products: {
+            type: Array as () => Product[],
+            required: true,
+        },
+    },
+    data() {
+        return {
+            currentSlideIndex: 0,
+        };
+    },
+    methods: {
+        exchangeBannerInfo(index: number) {
+            this.currentSlideIndex = index;
+        },
+    },
+})
+</script>
+
+<style scoped lang="css" src="./styles.css"></style>
---
+++ src/components/Carousel/CarouselPanel/CarouselPanel.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="carousel-controls-container">
+        <div v-for="(product, index) in productDetails" :key="product.id" @click="clickedControlProductIndex(index)">
+            <img :src="product.productImage" />
+        </div>
+    </div>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Product } from "../interfaces/ProductInterface";
+
+export default defineComponent({
+    name: "CarouselPanel",
+    props: {
+        productDetails: {
+            type: Array as () => Product[],
+            required: true,
+        },
+        currentProductId: Number,
+    },
+    methods: {
+        clickedControlProductIndex(index: number) {
+            this.$emit("clickedControlProductIndex", index);
+        },
+    },
+});
+</script>
+  
+<style scoped lang="css" src="./styles.css"></style>
